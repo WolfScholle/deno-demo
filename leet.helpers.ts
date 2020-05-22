0eef1233c60f69860652c722daa71cbf176ce3d8
Wolf Scholle <wolf.scholle@leanix.net>
2020-05-22 11:08:53 +0200

    Use date-fns

--- leet.helpers.ts
+++ leet.helpers.ts
@@ -1,4 +1,7 @@
-export function getTimeUntil1337(timeToCheck: Date) {
+import { intervalToDuration } from "./deps.ts";
+import { Duration } from "./leet.models.ts";
+
+export function getTimeUntil1337(timeToCheck: Date): Duration {
   const next1337 = new Date();
   next1337.setHours(13, 37);
 
@@ -6,18 +9,21 @@ export function getTimeUntil1337(timeToCheck: Date) {
     next1337.setDate(next1337.getDate() + 1);
   }
 
-  const timeUntil1337 = new Date(next1337.getTime() - timeToCheck.getTime());
+  const timeUntil1337 = intervalToDuration({
+    start: timeToCheck,
+    end: next1337,
+  });
   return timeUntil1337;
 }
 
-export function getStringUntil1337(timeUntil1337: Date) {
+export function getStringUntil1337(timeUntil1337: Duration) {
   if (timeExpired(timeUntil1337)) {
     return "It is 1337! 🎉";
   } else {
-    return `${timeUntil1337.getHours()} hours and ${timeUntil1337.getMinutes()} minutes to 1337!`;
+    return `${timeUntil1337.hours} hours and ${timeUntil1337.minutes} minutes to 1337!`;
   }
 }
 
-export function timeExpired(possible1337: Date): boolean {
-  return possible1337.getHours() === 0 && possible1337.getMinutes() === 0;
+export function timeExpired(possible1337: Duration): boolean {
+  return possible1337.hours === 0 && possible1337.minutes === 0;
 }
---
+++ leet.models.ts
@@ -0,0 +1,8 @@
+interface Duration {
+  years: number;
+  months: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
