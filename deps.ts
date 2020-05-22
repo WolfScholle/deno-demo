51ded2b3ab490d9c93d1791f8e6d03dafa163246
Wolf Scholle <wolf.scholle@leanix.net>
2020-05-22 09:14:48 +0200

    Add comment to deps.ts for unusable imports

--- deps.ts
+++ deps.ts
@@ -1,1 +1,5 @@
 export * from "https://deno.land/std@v0.50.0/http/server.ts";
+
+// This is not possible because the TypeScript file imports like node.js. So extension is missing.
+// // @deno-types="https://github.com/date-fns/date-fns/blob/master/src/intervalToDuration/index.d.ts"
+// export { intervalToDuration } from "https://github.com/date-fns/date-fns/blob/master/src/intervalToDuration/index.js";
