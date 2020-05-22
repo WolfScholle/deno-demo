f4697f6dcae4bec0a1a031e3547177ed45e051bf
Wolf Scholle <wolf.scholle@leanix.net>
2020-05-22 14:03:25 +0200

    Add headers to response for supporting emojis

--- main.ts
+++ main.ts
@@ -5,5 +5,10 @@ const server = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 
 for await (const request of server) {
-  request.respond({ body: getStringUntil1337(getTimeUntil1337(new Date())) });
+  const headers = new Headers();
+  headers.append("Content-Type", "text/html; charset=UTF-8");
+  request.respond({
+    headers,
+    body: getStringUntil1337(getTimeUntil1337(new Date())),
+  });
 }
