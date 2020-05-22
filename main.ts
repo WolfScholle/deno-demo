7693f8bbfea0683a2384ad091b33716e1f2ba3d0
Wolf Scholle <wolf.scholle@leanix.net>
2020-05-22 08:43:31 +0200

    Implement simple server to serve time until 1337

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
+import { serve } from "./deps.ts";
 import { getStringUntil1337, getTimeUntil1337 } from "./leet.helpers.ts";
 
-function printTimeTo1377() {
-  console.clear();
-  console.log(getStringUntil1337(getTimeUntil1337(new Date())));
+const s = serve({ port: 8000 });
+console.log("http://localhost:8000/");
+
+for await (const req of s) {
+  req.respond({ body: getStringUntil1337(getTimeUntil1337(new Date())) });
 }
-
-printTimeTo1377();
-
-setInterval(printTimeTo1377, 1 * 60 * 1000);
