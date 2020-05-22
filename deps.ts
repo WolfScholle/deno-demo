ddd801151f039e580b34da5c67f268e1ec172787
Wolf Scholle <wolf.scholle@leanix.net>
2020-05-22 11:08:38 +0200

    Add date-fns dependency

--- deps.ts
+++ deps.ts
@@ -1,5 +1,7 @@
 export * from "https://deno.land/std@v0.50.0/http/server.ts";
 
-// This is not possible because the TypeScript file imports like node.js. So extension is missing.
-// // @deno-types="https://github.com/date-fns/date-fns/blob/master/src/intervalToDuration/index.d.ts"
-// export { intervalToDuration } from "https://github.com/date-fns/date-fns/blob/master/src/intervalToDuration/index.js";
+// funny thing, this typing seems to work. even though it uses "import { intervalToDuration } from 'date-fns'" without suffix
+// this may be, because it is just used for typing and not by compiler...
+// ... or the previous attempt was only complaining about the .js file
+/// <reference path="https://deno.land/x/date_fns/intervalToDuration/index.d.ts" />
+export { intervalToDuration } from "https://deno.land/x/date_fns/index.js";
